8f5aa8b55e929348f5b9bea765e2d08132c7aa60
gblhjh <10700895+huang-jiahui-code@user.noreply.gitee.com>
2024-09-13 17:32:58 +0800

    init base module

---
+++ init/base/src/main/java/me/xgwd/base/resp/IErrorCode.java
@@ -0,0 +1,21 @@
+package me.xgwd.base.resp;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author:
+ * @Date: 2024/09/13/17:26
+ * @Description:
+ */
+public interface IErrorCode {
+
+    /**
+     * 错误码
+     */
+    String code();
+
+    /**
+     * 错误信息
+     */
+    String message();
+}
